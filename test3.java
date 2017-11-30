59fe783352412bf917cbd6bbea3d3d848dcf980d
nauilly <31853011+nauilly@users.noreply.github.com>
2017-11-30 22:23:55 +0800

    第一次上传文件到Github
    上传了一个test3.java文件

---
+++ test3.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+public class test3{
+  public static void main(String args[]){
+  int number=0;
+  System.out.println("输入正整数(按回车确认)");
+  Scanner reader = new Scanner(System.in);
+  number = reader.nextInt();
+  switch(number){
+  case 36 : 
+  case 58 :  System.out.println(number+"为三等奖");
+  case259 :  
+  break;
+  case 879 :
+  case 3 :
+  case 1:  System.out.println(number+"是二等奖");
+  break;
+  case 2 :
+  case 5 :
+  case 9 :  System.out.println(number+"是一等奖");
+  default: System.out.println(number+"未中奖，谢谢"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
